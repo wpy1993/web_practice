6d1d5a3f1478ece49b5dd45deec8665ac5c22ebf
wplay <yingxionguc@163.com>
2024-05-07 23:23:26 +0800

    feat: 2024-5-7

--- leetcode/array.js
+++ leetcode/array.js
@@ -39,5 +39,38 @@ const removeElement = (nums, val) => {
   return i; // 这个i已经是下一位了，不需要+1
 };
 
-const arr = [1, 1, 2, 4, 5, 2, 3, 3, 4];
+// const arr = [1, 1, 2, 4, 5, 2, 3, 3, 4];
 // removeElement(arr, 2);
+
+// 面试题 17.19. 消失的两个数字
+//  O(N) 时间内只用 O(1) 的空间
+// 参考题目 - 1-100中寻找失去的两个数
+var missingTwo = function (nums) {
+  const allLens = nums.length + 2;
+
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+
+  const missingSum = (allLens * (1 + allLens)) / 2 - sum;
+  const halfMissingNum = missingSum / 2;
+
+  let leftSum = 0;
+  let leftLens = 0;
+  for (let num of nums) {
+    if (num < halfMissingNum) {
+      leftSum += num;
+      leftLens++;
+    }
+  }
+  leftLens++;
+
+  const preSum = (leftLens * (1 + leftLens)) / 2;
+  const leftOne = preSum - leftSum;
+
+  return [leftOne, missingSum - leftOne];
+};
+
+// const arr = [1];
+// console.log(missingTwo(arr));
